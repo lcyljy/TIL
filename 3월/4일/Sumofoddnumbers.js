86d0550b93a2b0b151418caee6c0086918213633
Chang_young Lee <lodin_love@naver.com>
2022-03-04 19:37:36 +0900

    Create Sumofoddnumbers.js

---
+++ 3월/4일/Sumofoddnumbers.js
@@ -0,0 +1,25 @@
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+*/
+
+// # Solution
+
+function rowSumOddNumbers(n) {
+	// TODO
+  // 1 = 1
+  // 2 = 1(+2) + 1(+2+2)
+  // 1= 1 2 = 8 3 = 27 4 = 64
+  // n = n^3?
+  return n**3;
+}
